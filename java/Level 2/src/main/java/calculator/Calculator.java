cfccb5c347877f151f4eb0cfc9bf5a01269c503a
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-07-07 21:17:32 +0900

    Level2 - 2

--- java/Level 2/src/main/java/calculator/Calculator.java
+++ java/Level 2/src/main/java/calculator/Calculator.java
@@ -1,27 +1,29 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
-    private Integer results = 0;
+    private List<Integer> results = new ArrayList<>();
 
     public Integer calculate(int a, int b, char operator) {
-        switch (operator) {
-            case '+':
-                results = a + b;
-                break;
-            case '-':
-                results = a - b;
-                break;
-            case '*':
-                results = a * b;
-                break;
-            case '/':
-                if(b == 0) throw new ArithmeticException("0으로 나눌 수 없습니다.");
-                results = a / b;
-                break;
-            default:
-                results = 0;
-        }
+        return switch (operator) {
+            case '+' -> a + b;
+            case '-' -> a - b;
+            case '*' -> a * b;
+            case '/' -> {
+                if (b == 0) throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                yield a / b;
+            }
+            default -> 0;
+        };
+    }
+
+    public List<Integer> getResults() {
         return results;
     }
 
+    public void setResults(List<Integer> nums) {
+        this.results = nums;
+    }
 }
