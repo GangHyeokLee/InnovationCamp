9f412f55fb26692154c98b6f1620df30987aea06
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-07-17 21:27:28 +0900

    Feat: Memo update, delete api

--- spring/week1/memo/src/main/java/com/example/memo/controller/MemoController.java
+++ spring/week1/memo/src/main/java/com/example/memo/controller/MemoController.java
@@ -39,4 +39,32 @@ public class MemoController {
         // Map to List
         return memoList.values().stream().map(MemoResponseDto::new).toList();
     }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable(value = "id") Long id, @RequestBody MemoRequestDto requestDto) {
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 해당 메모 가져오기
+            Memo memo = memoList.get(id);
+
+            // memo 수정
+            memo.update(requestDto);
+
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("Memo does not exist");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable(value = "id") Long id) {
+        // 해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // 해당 메모를 삭제하기
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("Memo does not exist");
+        }
+    }
 }
