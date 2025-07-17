21b99623917154052d5802a4e274c04324a9dc43
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-07-17 21:16:52 +0900

    Feat: Memo create, read api

---
+++ spring/week1/memo/src/main/java/com/example/memo/controller/MemoController.java
@@ -0,0 +1,42 @@
+package com.example.memo.controller;
+
+import com.example.memo.dto.MemoRequestDto;
+import com.example.memo.dto.MemoResponseDto;
+import com.example.memo.entity.Memo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
+public class MemoController {
+
+    private final Map<Long, Memo> memoList = new HashMap<>();
+
+    @PostMapping("/memos")
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+
+        //  RequestDto -> Entity
+        Memo memo = new Memo(requestDto);
+
+        // Memo Max ID Check
+        Long maxId = !memoList.isEmpty() ? Collections.max(memoList.keySet()) + 1 : 1;
+        memo.setId(maxId);
+
+        // DB 저장
+        memoList.put(memo.getId(), memo);
+
+        // Entity -> ResponstDto
+        return new MemoResponseDto(memo);
+    }
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos() {
+        // Map to List
+        return memoList.values().stream().map(MemoResponseDto::new).toList();
+    }
+}
