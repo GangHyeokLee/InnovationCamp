d48ed6e6a89574d590d4fd7fa18fbd9a9fdc6af3
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-07-18 20:50:45 +0900

    Fix: Integrate Jpa

--- spring/week1/memo/src/main/java/com/example/memo/entity/Memo.java
+++ spring/week1/memo/src/main/java/com/example/memo/entity/Memo.java
@@ -2,16 +2,25 @@ package com.example.memo.entity;
 
 
 import com.example.memo.dto.MemoRequestDto;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@Table(name = "memo")
 @NoArgsConstructor
 public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "username", nullable = false)
     private String username;
+
+    @Column(name = "contents", nullable = false, length = 500)
     private String contents;
 
     public Memo(MemoRequestDto requestDto) {
