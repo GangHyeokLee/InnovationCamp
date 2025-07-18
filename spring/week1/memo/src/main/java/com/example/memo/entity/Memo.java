b0bee37016a4928f124ee3065e4769a0c3243d91
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-07-18 20:57:12 +0900

    Feat: JPA auditing for timestamp

--- spring/week1/memo/src/main/java/com/example/memo/entity/Memo.java
+++ spring/week1/memo/src/main/java/com/example/memo/entity/Memo.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 @Setter
 @Table(name = "memo")
 @NoArgsConstructor
-public class Memo {
+public class Memo extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
