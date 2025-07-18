6ecf52cf54067f73b77bfb03f17ca4af9d791232
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-07-18 11:54:16 +0900

    Feat: Separate controller, service, and repository.

---
+++ spring/week1/memo/src/main/java/com/example/memo/service/MemoService.java
@@ -0,0 +1,52 @@
+package com.example.memo.service;
+
+import com.example.memo.dto.MemoRequestDto;
+import com.example.memo.dto.MemoResponseDto;
+import com.example.memo.entity.Memo;
+import com.example.memo.repository.MemoRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemoService {
+    private final MemoRepository memoRepository;
+
+    public MemoService(JdbcTemplate jdbcTemplate) {
+        this.memoRepository = new MemoRepository(jdbcTemplate);
+    }
+
+    public MemoResponseDto createMemo(MemoRequestDto requestDto) {
+        // RequestDto -> Entity
+        return new MemoResponseDto(memoRepository.save(new Memo(requestDto)));
+    }
+
+    public List<MemoResponseDto> getMemos() {
+        return memoRepository.findAll();
+    }
+
+    public Long updateMemo(Long id, MemoRequestDto requestDto) {
+        // 해당 메모가 DB에 존재하는지 확인
+        Memo memo = memoRepository.findById(id);
+        if (memo != null) {
+            // memo 내용 수정
+            memoRepository.update(id, requestDto);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+
+    public Long deleteMemo(Long id) {
+        // 해당 메모가 DB에 존재하는지 확인
+        Memo memo = memoRepository.findById(id);
+        if (memo != null) {
+            memoRepository.delete(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+}
