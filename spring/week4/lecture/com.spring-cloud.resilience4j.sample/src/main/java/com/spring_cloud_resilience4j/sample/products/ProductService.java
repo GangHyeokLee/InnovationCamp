f3ba1f2e09993200fe48c2fc0813554bcada8a75
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-07-31 21:43:28 +0900

    Resilience4j Exercise Init

---
+++ spring/week4/lecture/com.spring-cloud.resilience4j.sample/src/main/java/com/spring_cloud_resilience4j/sample/products/ProductService.java
@@ -0,0 +1,24 @@
+package com.spring_cloud_resilience4j.sample.products;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackGetProductDetails")
+    public Product getProductDetails(String productId) {
+
+        if ("111".equals(productId)) {
+            throw new RuntimeException("Empty response body");
+        }
+
+        return new Product(productId, "Product " + productId);
+    }
+
+    public Product fallbackGetProductDetails(String productId, Throwable t) {
+        return new Product(productId, "Fallback Product " + productId);
+    }
+}
