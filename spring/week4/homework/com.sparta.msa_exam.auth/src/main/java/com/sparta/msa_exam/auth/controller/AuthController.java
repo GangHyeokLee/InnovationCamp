2abde6e4b466d1b163213a6c306ff88f7daa537a
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-08-01 14:28:00 +0900

    feat(auth): add auth service skeleton code (not working yet)

---
+++ spring/week4/homework/com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/controller/AuthController.java
@@ -0,0 +1,20 @@
+package com.sparta.msa_exam.auth.controller;
+
+import com.sparta.msa_exam.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @GetMapping("/auth/sign-in")
+    public ResponseEntity<String> signIn(@RequestParam String user-id) {
+
+    }
+}
---
+++ spring/week4/homework/com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/service/AuthService.java
@@ -0,0 +1,4 @@
+package com.sparta.msa_exam.auth.service;
+
+public class AuthService {
+}
