46c239d3694b7c2dedaf13c12d71e6ab36e3cd17
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-08-02 09:24:56 +0900

    feat(order): add ProductClient and ProductResponseDto for product service integration

---
+++ spring/week4/homework/com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/dto/response/ProductResponseDto.java
@@ -0,0 +1,11 @@
+package com.sparta.msa_exam.order.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class ProductResponseDto {
+    private Long id;
+    private String name;
+    private Integer supply_price;
+
+}
