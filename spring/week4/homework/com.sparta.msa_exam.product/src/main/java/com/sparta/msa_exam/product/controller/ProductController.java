7d2323d91d83a9979915aad5b13f1b57a2b92e2b
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-08-02 13:33:56 +0900

    test(product): add port response controller

--- spring/week4/homework/com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
+++ spring/week4/homework/com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.sparta.msa_exam.product.dto.request.ProductSaveRequestDto;
 import com.sparta.msa_exam.product.dto.response.ProductResponseDto;
 import com.sparta.msa_exam.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,8 +15,10 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
-
     private final ProductService productService;
+
+    @Value("${server.port}")
+    private String serverPort;
 
     @GetMapping
     public ResponseEntity<List<ProductResponseDto>> getProducts(){
@@ -25,5 +28,11 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<ProductResponseDto> saveProduct(@RequestBody ProductSaveRequestDto productSaveRequestDto){
         return ResponseEntity.ok(productService.saveProduct(productSaveRequestDto));
+    }
+
+    // 로드밸런싱 테스트용
+    @GetMapping("/port")
+    public ResponseEntity<String> getPort(){
+        return ResponseEntity.ok().body(serverPort);
     }
 }
