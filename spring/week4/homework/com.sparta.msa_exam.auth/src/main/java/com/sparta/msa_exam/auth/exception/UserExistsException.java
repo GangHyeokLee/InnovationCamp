31ad4fb96171c6b9d77d3b5850db6c01a57548f8
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2025-08-02 12:43:18 +0900

    refactor(auth): refactor unnecessary code

--- spring/week4/homework/com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/exception/UserExistsException.java
+++ spring/week4/homework/com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/exception/UserExistsException.java
@@ -1,12 +1,7 @@
 package com.sparta.msa_exam.auth.exception;
 
 public class UserExistsException extends RuntimeException {
-    public UserExistsException(String message) {
-        super(message);
-    }
-
     public UserExistsException() {
         super("User already exists");
     }
-
 }
